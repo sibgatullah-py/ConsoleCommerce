67f316361c908cd5a708d904b258a6e16340287b
Sibgatullah <sibgatullah881@gmail.com>
2025-10-30 21:56:36 +0600

    made a dunder method for database

--- core/models/user.py
+++ core/models/user.py
@@ -3,3 +3,10 @@
 from datetime import datetime
 from core.utils import verify_password # since we aren't hashing the password so we only need this method 
 
+class User:
+    """Handles user data and database interactions."""
+    
+    def __init__(self,db):
+        self.db = db
+        
+        
