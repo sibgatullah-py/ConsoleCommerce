e2bd68bc67ac3eef97d285634b255e91fcd0e647
Sibgatullah <sibgatullah881@gmail.com>
2025-10-31 20:19:39 +0600

    fixed a typo in database.py file . took access to the database methods with self.db in constructor

--- core/database.py
+++ core/database.py
@@ -42,7 +42,7 @@ class DatabaseManager:
         
         # products table
         self.cursor.execute("""
-                            CRATE TABLE IF NOT EXISTS products (
+                            CREATE TABLE IF NOT EXISTS products (
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 name TEXT NOT NULL,
                                 price REAL NOT NULL,
--- core/models/product.py
+++ core/models/product.py
@@ -1,2 +1,9 @@
 # core/models/product.py
 
+from datetime import datetime
+
+class Product:
+    '''Handles all product-related database operations.'''
+    
+    def __init__(self,db):# db is the DatabaseManager instance, giving access to .execute() just like the cursor 
+        self.db = db
