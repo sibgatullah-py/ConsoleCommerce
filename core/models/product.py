52c846667dfb58900f538d902c096c7561bf0b5a
Sibgatullah <sibgatullah881@gmail.com>
2025-10-31 20:29:00 +0600

    added Create method for adding new products in the database

--- core/models/product.py
+++ core/models/product.py
@@ -7,3 +7,21 @@ class Product:
     
     def __init__(self,db):# db is the DatabaseManager instance, giving access to .execute() just like the cursor 
         self.db = db
+        
+    # Now the CRUD system starts from here --->
+    
+    # ----- CREATE ----->
+    def add_product(self,name:str,price:float,stock: int = 0,description: str = "") -> bool:
+        '''Add a new product in the database table (products)'''
+        created_at = datetime.now().isoformat(timespec="seconds")
+        
+        self.db.execute("""
+                        INSERT INTO products (name, price, stock, description)
+                        VALUES (?,?,?,?)
+                        """,
+                        (name, float(price), int(stock), description),
+                        commit = True
+                        )
+        
+        print(f"Product {name} added successfully!")
+        return True
