76f37ec89a7344124cab4a65a903305b010ab34d
Sibgatullah <sibgatullah881@gmail.com>
2025-11-04 22:19:53 +0600

    CartService class

--- core/services/cart_service.py
+++ core/services/cart_service.py
@@ -6,3 +6,18 @@ This module will handle
     
 It will work directly with the Product and Order models and use DatabaseManager for query
 '''
+"""
+Handles suer cart operations and order creation.
+Each user's cart is stored in memory wile the app runs.
+When they checkout, the items are stored in the 'orders' table.
+"""
+
+import json
+from datetime import datetime
+from core.models.product import Product
+from core.models.order import Order
+
+class CartService:
+    def __init__(self,db):
+        self.db = db
+        self.carts = {} # in_memory carts: {user_id:{product_id: qty,....}}
