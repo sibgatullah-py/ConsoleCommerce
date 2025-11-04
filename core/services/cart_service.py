7cf26365f0ff9171c2d85d83b5c238ec475bb58e
Sibgatullah <sibgatullah881@gmail.com>
2025-11-04 22:06:30 +0600

    started working on cat_service module

---
+++ core/services/cart_service.py
@@ -0,0 +1,8 @@
+'''
+This module will handle
+    1. Adding/removing items to a user's cart
+    2. Viewing cart contents
+    3. Checking out (creating an order record in the database)
+    
+It will work directly with the Product and Order models and use DatabaseManager for query
+'''
